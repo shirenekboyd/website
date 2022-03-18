e6f249fe92eacea8cbff05e01687dd0428dbe5ed
Shirene Boyd <shirenekadkhodai@gmail.com>
2022-03-18 11:28:21 -0500

    updated with new files for portfolio items and js functionality

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+//querySelectorAll looks for all nav links
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', ()=> {
-        document.body.classList.remove('nav-open');
-    })
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
 });
